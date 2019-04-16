9ff5a5b486522b7b5d45455dadd774ec1247ad8b
xuxun_glb <xuxunxuxun_glb@atguigu.com>
2019-04-16 21:36:32 +0800

    修改

--- src/com/atguigu/java/HelloWorld.java
+++ src/com/atguigu/java/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
     private static final Customer customer = new Customer();
 
     public static void main(String[] args) {
-        Season spring = Season.AUTUMN;
+        Season spring  = Season.AUTUMN;
         System.out.println(spring.getSeasonName());
         String[] str = new String[]{"Tom", "lisi", "zhangsan"};
         for (String s : str) {
