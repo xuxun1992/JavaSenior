276dab15e5ddff3cd532f2ecc44aade2aed770e4
xuxun_glb <xuxunxuxun_glb@atguigu.com>
2019-04-16 21:46:02 +0800

    修改李四

--- src/com/atguigu/java/HelloWorld.java
+++ src/com/atguigu/java/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         Season spring  = Season.AUTUMN;
         System.out.println(spring.getSeasonName());
-        String[] str = new String[]{"Tom", "lisi", "zhangsan","王五"};
+        String[] str = new String[]{"Tom", "lisi", "zhangsan","王五","李四"};
         for (String s : str) {
             System.out.println(s);
         }
